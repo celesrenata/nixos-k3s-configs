6a36917c04b9ff335463abe88e7045febf6c1b1e
Celes Renata <celes@celestium.life>
2024-12-08 23:15:10 -0800

    update unifi

--- unifi/runmefirst.sh
+++ unifi/runmefirst.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 kubectl create namespace unifi-service
+kubectl apply -k config/rbac/  --namespace unifi-service
+kubectl apply -f mongo.yaml -n unifi-service 
+echo "Creating mongodb"
+sleep 60
 kubectl kustomize . | kubectl create -n unifi-service -f -
