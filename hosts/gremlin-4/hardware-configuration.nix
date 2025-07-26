030ad6c8688fd7a7e7daec00cb14227a27bb2f02
Celes Renata <celes@frameshift.net>
2025-07-25 23:25:12 -0700

    Add gremlin-4 template hardware config and fix gitignore
    - Updated .gitignore to only block hardware-configuration.nix in root
    - Added template hardware-configuration.nix for gremlin-4 with placeholder UUIDs
    - Ready for deployment when gremlin-4 hardware is available

---
+++ hosts/gremlin-4/hardware-configuration.nix
@@ -0,0 +1,39 @@
+# Template hardware configuration for gremlin-4
+# This will need to be replaced with actual hardware-configuration.nix
+# when gremlin-4 is deployed
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  # PLACEHOLDER - Replace with actual filesystem configuration when deploying gremlin-4
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/PLACEHOLDER-UUID-FOR-GREMLIN-4";
+      fsType = "btrfs";
+      options = [ "compress=zstd" "subvol=root" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/PLACEHOLDER-BOOT-UUID-FOR-GREMLIN-4";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
