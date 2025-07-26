e362731d2c5d5341609a1fd85f5bfe38d324c989
Celes Renata <celes@frameshift.net>
2025-07-26 01:57:57 -0700

    Fix UPS configuration for multi-node setup
    - gremlin-1: UPS server mode with physical UPS connection
    - gremlin-2/gremlin-3: UPS client mode connecting to gremlin-1 at 10.1.1.12:3493
    - Conditional configuration based on hostname to prevent UPS service errors
    - Only gremlin-1 gets USB device rules and server-specific systemd services

--- modules/ups.nix
+++ modules/ups.nix
@@ -1,10 +1,15 @@
 
+{ config, lib, ... }:
 let
   vid = "051D";
   pid = "0002";
   upsname = "apcsmx1500-a";
   pass-master = "masterMonitorPassword"; # master password for nut
   pass-local = "localMonitorPassword"; # slave/local password for nut
+  
+  # gremlin-1 has the physical UPS and acts as server
+  # gremlin-2 and gremlin-3 are clients
+  isUpsServer = (config.networking.hostName == "gremlin-1");
 in
 {
   # at some point something will make a /var/state/ups directory,
@@ -12,39 +17,54 @@ in
   # $ sudo chown nut:nut /var/state/ups
   power.ups = {
     enable = true;
-    users.nutmaster = {
-      upsmon = "primary";
-      passwordFile = "/etc/nixos/.config/PasswordFiles/apc.pass";
+    
+    # Server configuration (gremlin-1 only)
+    users = lib.mkIf isUpsServer {
+      nutmaster = {
+        upsmon = "primary";
+        passwordFile = "/etc/nixos/.config/PasswordFiles/apc.pass";
+      };
     };
-    mode = "netserver";
-    schedulerRules = "/etc/nixos/.config/nut/upssched.conf";
+    
+    mode = if isUpsServer then "netserver" else "netclient";
+    
+    schedulerRules = lib.mkIf isUpsServer "/etc/nixos/.config/nut/upssched.conf";
+    
     upsmon.monitor.apcsmx1500-a = {
       powerValue = 1;
       user = "nutmaster";
       passwordFile = "/etc/nixos/.config/PasswordFiles/apc.pass";
+      # For clients, specify the remote system
+      system = lib.mkIf (!isUpsServer) "apcsmx1500-a@10.1.1.12:3493";
     };
+    
+    # Physical UPS configuration (gremlin-1 only)
     # debug by calling the driver:
     # $ sudo NUT_CONFPATH=/etc/nut/ usbhid-ups -u nut -D -a apcsmx1500-a
-    ups.${upsname} = {
-      # find your driver here:
-      # https://networkupstools.org/docs/man/usbhid-ups.html
-      driver = "usbhid-ups";
-      shutdownOrder = 0;
-      description = "APC SMX1500RM2UNC UPS";
-      port = "auto";
-      directives = [
-        "vendorid = ${vid}"
-        "productid = ${pid}"
-        "ignorelb"
-        "override.battery.charge.low = 50"
-        "override.battery.runtime.low = 1200"
-      ];
-      # this option is not valid for usbhid-ups
-      maxStartDelay = null;
+    ups = lib.mkIf isUpsServer {
+      ${upsname} = {
+        # find your driver here:
+        # https://networkupstools.org/docs/man/usbhid-ups.html
+        driver = "usbhid-ups";
+        shutdownOrder = 0;
+        description = "APC SMX1500RM2UNC UPS";
+        port = "auto";
+        directives = [
+          "vendorid = ${vid}"
+          "productid = ${pid}"
+          "ignorelb"
+          "override.battery.charge.low = 50"
+          "override.battery.runtime.low = 1200"
+        ];
+        # this option is not valid for usbhid-ups
+        maxStartDelay = null;
+      };
     };
+    
     maxStartDelay = 30;
   };
 
+  # User and group configuration (all nodes need this)
   users = {
     users.nut = {
       isSystemUser = true;
@@ -57,23 +77,29 @@ in
     groups.nut = { };
   };
 
-  services.udev.extraRules = ''
+  # USB device rules (gremlin-1 only, since it has the physical UPS)
+  services.udev.extraRules = lib.mkIf isUpsServer ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="${vid}", ATTRS{idProduct}=="${pid}", MODE="664", GROUP="nut", OWNER="nut"
   '';
 
-  systemd.services.upsd.serviceConfig = {
-    User = "root";
-    Group = "nut";
+  # Server-specific systemd service configuration (gremlin-1 only)
+  systemd.services = lib.mkIf isUpsServer {
+    upsd.serviceConfig = {
+      User = "root";
+      Group = "nut";
+    };
+    upsdrv.serviceConfig = {
+      User = "root";
+      Group = "nut";
+    };
   };
-  power.ups.upsd.listen = [
+  
+  # Network listener configuration (gremlin-1 only)
+  power.ups.upsd.listen = lib.mkIf isUpsServer [
     {
       address = "0.0.0.0";
       port = 3493;
     }
   ];
-  systemd.services.upsdrv.serviceConfig = {
-    User = "root";
-    Group = "nut";
-  };
 }
 
