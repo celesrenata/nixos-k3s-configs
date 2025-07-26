3cb42fec27edc2a1a03f9882c598a6e89f82fc1e
Celes Renata <celes@frameshift.net>
2025-07-25 23:15:31 -0700

    Fix .gitignore and add missing boot.nix and remote-build.nix modules
    - Updated .gitignore to only block legacy files in root directory
    - Added modules/boot.nix for boot configuration
    - Added modules/remote-build.nix for distributed builds

---
+++ modules/boot.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.initrd.systemd.enable = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = false;
+    };
+    grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+  };
+  # KMS Module loading
+  boot.initrd.kernelModules = [ "vmd" "md_mod" "raid0" ];
+  boot.crashDump.enable = true;
+  
+  # See Kernel Overlay
+  boot.kernelPackages = pkgs.kernelPXP;
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  boot.kernelModules = [ "i915" ];
+  boot.supportedFilesystems = [ "nfs" ];
+  
+  # Setup SR-IOV Required Parameters for Arc iGPU
+  boot.kernelParams = [
+    "intel_iommu=on"
+    "iommu=pt"
+    "i915.enable_guc=3"
+    "i915.max_vfs=7"
+    "i915.force_probe=7d55"
+    "module_blacklist=xe"  
+    "boot.shell_on_fail"
+    "hugepagesz=1G"
+    "hugepages=2"
+    "hugepagesz=2M"
+    "hugepages=512"
+  ];
+  
+  # SR-IOV Module - now handled by graphics modules
+  # boot.extraModulePackages = with pkgs; [ i915-sriov ];
+
+  # Kubernetes FS problem solver
+  boot.kernel.sysctl."fs.inotify.max_user_instances" = 2147483647;
+}
---
+++ modules/remote-build.nix
@@ -0,0 +1,43 @@
+{ config, ... }:
+{
+  nix.distributedBuilds = true;
+  nix.settings.system-features = [
+    "kvm"
+    "big-parallel"
+    "nixos-test"
+    "benchmark"
+  ];
+  
+  # Configure gremlin-2, gremlin-3, and gremlin-4 as remote build servers
+  # Intel Core Ultra 9 185H: 16 cores (6P+8E+2LP), 22 threads, 96GB RAM, RAID0 NVMe
+  # Configured for 50% CPU utilization = ~11 threads
+  nix.buildMachines = [
+    {
+      hostName = "gremlin-2";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 11;  # 50% of 22 threads
+      speedFactor = 3; # High performance: 96GB RAM + RAID0 NVMe + modern CPU
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }
+    {
+      hostName = "gremlin-3";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 11;  # 50% of 22 threads
+      speedFactor = 3; # High performance: 96GB RAM + RAID0 NVMe + modern CPU
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }
+    {
+      hostName = "gremlin-4";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 11;  # 50% of 22 threads
+      speedFactor = 3; # High performance: 96GB RAM + RAID0 NVMe + modern CPU
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }
+  ];
+}
