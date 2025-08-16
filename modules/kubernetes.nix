61936e7181bd03afc0b623f249adf335645285be
Celes Renata <celes@frameshift.net>
2025-08-16 03:33:28 -0700

    new multus cni attempt

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -1,20 +1,11 @@
 { config, lib, pkgs, hasNvidia ? false, ... }:
-let
-  fullCNIPlugins = pkgs.buildEnv {
-    name = "cni-full";
-    paths = with pkgs; [
-      cni-plugin-flannel
-      multus-cni
-      cni-plugins
-    ];
-  };
-in
 {
   environment.systemPackages = with pkgs; [
     docker
     runc
     k3s 
     kubernetes-helm
+    multus-cni  # Make multus-cni available system-wide
   ] ++ lib.optionals hasNvidia [
     nvidia-container-toolkit
     libnvidia-container
@@ -26,39 +17,20 @@ in
     token = "532a3cf6ea";
     clusterInit = (config.networking.hostName == "gremlin-1");
     serverAddr = lib.mkIf (config.networking.hostName != "gremlin-1") "https://10.1.1.12:6443";
-    # Remove flannel-backend=none - let K3s use its default Flannel
-    extraFlags = (toString [
+    # Let K3s use its default Flannel
+    extraFlags = toString [
       "--container-runtime-endpoint unix:///run/containerd/containerd.sock"
-    ]); 
+    ]; 
   };
 
-  systemd.services.k3s-containerd-setup = {
-    serviceConfig.Type = "oneshot";
-    requiredBy = ["k3s.service"];
-    before = ["k3s.service"];
-    script = ''
-      mkdir -p /var/lib/rancher/k3s/agent/etc/containerd
-      mkdir -p /var/lib/rancher/k3s/data/cni
-      
-      # Copy CNI binaries to the K3s CNI directory
-      cp -r ${fullCNIPlugins}/bin/* /var/lib/rancher/k3s/data/cni/
-      
-      cat << EOFCONFIG > /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl
-      {{ template "base" . }}
-      version = 2
-
-      [plugins."io.containerd.grpc.v1.cri".cni]
-        bin_dir  = "/var/lib/rancher/k3s/data/cni"
-        conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d"
-
-      [plugins."io.containerd.grpc.v1.cri".containerd]
-        default_runtime_name = "runc"
-
-        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-          runtime_type = "io.containerd.runc.v2"
-      EOFCONFIG
-    '';
-  };
+  # Create a proper CNI directory with all needed binaries
+  systemd.tmpfiles.rules = [
+    "d /var/lib/rancher/k3s/data/cni 0755 root root -"
+    "L+ /var/lib/rancher/k3s/data/cni/multus - - - - ${pkgs.multus-cni}/bin/multus"
+    "L+ /var/lib/rancher/k3s/data/cni/multus-daemon - - - - ${pkgs.multus-cni}/bin/multus-daemon"
+    "L+ /var/lib/rancher/k3s/data/cni/multus-shim - - - - ${pkgs.multus-cni}/bin/multus-shim"
+    "L+ /var/lib/rancher/k3s/data/cni/thin_entrypoint - - - - ${pkgs.multus-cni}/bin/thin_entrypoint"
+  ];
 
   virtualisation = {
     docker = {
