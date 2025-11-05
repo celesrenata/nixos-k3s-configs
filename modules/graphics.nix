13be057a1e008fc3b9b8b2d1d3921a77382e866a
Celes Renata <celes@frameshift.net>
2025-11-04 18:55:38 -0800

    update nvidia drivers

--- modules/graphics.nix
+++ modules/graphics.nix
@@ -4,11 +4,11 @@ let
   cfg = config.gremlin.graphics;
   
   nvidia-package = config.boot.kernelPackages.nvidiaPackages.mkDriver ({
-    version = "580.95.05";
-    sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+    version = "580.105.08";
+    sha256_64bit = "sha256-2cboGIZy8+t03QTPpp3VhHn6HQFiyMKMjRdiV2MpNHU=";
     sha256_aarch64 = "";
-    openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-    settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+    openSha256 = "sha256-FGmMt3ShQrw4q6wsk8DSvm96ie5yELoDFYinSlGZcwQ=";
+    settingsSha256 = "sha256-YvzWO1U3am4Nt5cQ+b5IJ23yeWx5ud1HCu1U0KoojLY=";
     persistencedSha256 = "";
   });
 in {
