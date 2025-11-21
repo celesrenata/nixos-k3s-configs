50933c51371be5afc3a980a342eda4330e4c62f4
Celes Renata <celes@frameshift.net>
2025-11-20 19:03:12 -0800

    Add Gremlin-4!

--- hosts/gremlin-4/hardware-configuration.nix
+++ hosts/gremlin-4/hardware-configuration.nix
@@ -15,18 +15,36 @@
 
   # PLACEHOLDER - Replace with actual filesystem configuration when deploying gremlin-4
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/PLACEHOLDER-UUID-FOR-GREMLIN-4";
+    { device = "/dev/disk/by-uuid/5c20d972-fa0e-4455-805e-b56f42d7ca81";
       fsType = "btrfs";
       options = [ "compress=zstd" "subvol=root" ];
     };
 
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/5c20d972-fa0e-4455-805e-b56f42d7ca81";
+      fsType = "btrfs";
+      options = [ "compress=zstd" "subvol=nix" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/5c20d972-fa0e-4455-805e-b56f42d7ca81";
+      fsType = "btrfs";
+      options = [ "compress=zstd" "subvol=home" ];
+    };
+
+  fileSystems."/var/lib" =
+    { device = "/dev/disk/by-uuid/5c20d972-fa0e-4455-805e-b56f42d7ca81";
+      fsType = "btrfs";
+      options = [ "compress=zstd" "subvol=varlib" ];
+    };
+
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/PLACEHOLDER-BOOT-UUID-FOR-GREMLIN-4";
+    { device = "/dev/disk/by-uuid/9686-5EB2";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/b452cd91-d05f-413a-8fc3-633f27c4d851"; } ];
 
   # Enables DHCP on each ethernet interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
