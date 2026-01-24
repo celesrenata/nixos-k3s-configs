93fe5e8d9494e7aafca088b2aca83d223f98227e
Celes Renata <celes@frameshift.net>
2026-01-24 13:38:04 -0800

    add gcc15 to distcc

--- modules/distcc-triplet.nix
+++ modules/distcc-triplet.nix
@@ -3,16 +3,16 @@
 let
   gccWithTriplet = pkgs.runCommand "gcc-with-triplet" {} ''
     mkdir -p $out/bin
-    ln -s ${pkgs.gcc}/bin/gcc $out/bin/gcc
-    ln -s ${pkgs.gcc}/bin/g++ $out/bin/g++
-    ln -s ${pkgs.gcc}/bin/gcc $out/bin/x86_64-pc-linux-gnu-gcc
-    ln -s ${pkgs.gcc}/bin/g++ $out/bin/x86_64-pc-linux-gnu-g++
-    ln -s ${pkgs.gcc}/bin/gcc-ar $out/bin/gcc-ar
-    ln -s ${pkgs.gcc}/bin/gcc-nm $out/bin/gcc-nm
-    ln -s ${pkgs.gcc}/bin/gcc-ranlib $out/bin/gcc-ranlib
-    ln -s ${pkgs.gcc}/bin/gcc-ar $out/bin/x86_64-pc-linux-gnu-gcc-ar
-    ln -s ${pkgs.gcc}/bin/gcc-nm $out/bin/x86_64-pc-linux-gnu-gcc-nm
-    ln -s ${pkgs.gcc}/bin/gcc-ranlib $out/bin/x86_64-pc-linux-gnu-gcc-ranlib
+    ln -s ${pkgs.gcc15}/bin/gcc $out/bin/gcc
+    ln -s ${pkgs.gcc15}/bin/g++ $out/bin/g++
+    ln -s ${pkgs.gcc15}/bin/gcc $out/bin/x86_64-pc-linux-gnu-gcc
+    ln -s ${pkgs.gcc15}/bin/g++ $out/bin/x86_64-pc-linux-gnu-g++
+    ln -s ${pkgs.gcc15}/bin/gcc-ar $out/bin/gcc-ar
+    ln -s ${pkgs.gcc15}/bin/gcc-nm $out/bin/gcc-nm
+    ln -s ${pkgs.gcc15}/bin/gcc-ranlib $out/bin/gcc-ranlib
+    ln -s ${pkgs.gcc15}/bin/gcc-ar $out/bin/x86_64-pc-linux-gnu-gcc-ar
+    ln -s ${pkgs.gcc15}/bin/gcc-nm $out/bin/x86_64-pc-linux-gnu-gcc-nm
+    ln -s ${pkgs.gcc15}/bin/gcc-ranlib $out/bin/x86_64-pc-linux-gnu-gcc-ranlib
   '';
 in
 {
