901a2395bf05e03e92d1f46ab8cbdcee6cc36df8
Celes Renata <celes@frameshift.net>
2026-04-20 21:28:39 -0700

    strip proxy settings

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -75,15 +75,15 @@ in
     # Proxy and SSL configuration for container image pulls
     {
       k3s.environment = {
-        HTTP_PROXY = "http://192.168.42.1:3128";
-        HTTPS_PROXY = "http://192.168.42.1:3128";
-        NO_PROXY = "10.0.0.0/8,192.168.0.0/16,127.0.0.1,localhost,.svc,.cluster.local,.celestium.life";
+        # HTTP_PROXY = "http://192.168.42.1:3128"; # disabled - SSL filtering off
+        #         HTTPS_PROXY = "http://192.168.42.1:3128";
+        #         NO_PROXY = "10.0.0.0/8,192.168.0.0/16,127.0.0.1,localhost,.svc,.cluster.local,.celestium.life";
         SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
       };
       containerd.environment = {
-        HTTP_PROXY = "http://192.168.42.1:3128";
-        HTTPS_PROXY = "http://192.168.42.1:3128";
-        NO_PROXY = "10.0.0.0/8,192.168.0.0/16,127.0.0.1,localhost,.svc,.cluster.local,.celestium.life";
+        # HTTP_PROXY = "http://192.168.42.1:3128"; # disabled - SSL filtering off
+        #         HTTPS_PROXY = "http://192.168.42.1:3128";
+        #         NO_PROXY = "10.0.0.0/8,192.168.0.0/16,127.0.0.1,localhost,.svc,.cluster.local,.celestium.life";
         SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
       };
     }
